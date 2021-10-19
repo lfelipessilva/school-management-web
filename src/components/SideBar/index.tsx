7469440d88a55c5c5d9a940b2661adfb6edbbb54
luisfelipe <luis.felipeszsv@gmail.com>
2021-10-19 10:50:32 -0300

    finishing navbar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,12 +1,39 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import { Container } from './styles'
 
+import { IconContext } from 'react-icons';
+import { BsFillPersonFill } from 'react-icons/bs'
+import { GiTeacher } from 'react-icons/gi'
+import { SiGoogleclassroom } from 'react-icons/si'
+
 export const SideBar = () => {
+
+    const [sidebar, setSidebar] = useState(false)
+    
+    const showSidebar = () => setSidebar(!sidebar) 
+
     return (
-        <Container>
-            <nav>
-                <li><Link to="/student">Estudantes</Link></li>
-                <li><Link to="/teacher">Professores</Link></li>
+        <Container active={sidebar}>
+            <nav className='navbar'>
+                <IconContext.Provider value={{className: 'react-icons'}}>
+                
+                    <Link className='link-class' to="/student">
+                        <BsFillPersonFill/>
+                        <p>Estudantes</p>
+                    </Link>
+                
+                    <Link className='link-class' to="/teacher">
+                        <GiTeacher />
+                        <p>Professores</p>
+                    </Link>
+
+                    <Link className='link-class' to="/class">
+                        <SiGoogleclassroom />
+                        <p>Turmas</p>
+                    </Link> 
+                
+                </IconContext.Provider>
             </nav>
         </Container>
     )
--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -1,5 +1,43 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    width: 200px;
-`
+export const Container = styled.div<{active: boolean}>`
+
+    width: 90px;
+    transition: 300ms;
+
+    p { 
+        display: none;
+    }
+
+    &:hover {
+        width: 200px;
+        p { 
+            padding: 0px;
+            margin: 5px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: black;
+            font-size: 1.3rem;
+        }
+    }
+
+    .navbar {
+        height: 100vh;
+        background-color: #F4EBD0;
+    }
+
+    .react-icons {
+        margin: 5px;
+        height: 85px;
+        width: 85px;
+        color: black;
+    }
+
+    .link-class {
+        display: flex;
+        height: 90px;
+        text-decoration: none;
+    }
+
+`   
